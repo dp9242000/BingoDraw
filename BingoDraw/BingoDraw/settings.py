91f8916e0072330dcf1b93556e5663767c462dad
Donovan Grady <dp9242000@gmail.com>
2022-11-08 18:03:58 +0700

    configured mysqlserver
    known issue: the java used for the DateTimePicker is interfering with the navbar toggle

--- BingoDraw/BingoDraw/settings.py
+++ BingoDraw/BingoDraw/settings.py
@@ -76,17 +76,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BingoDraw.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BingoDrawTest',
+        'USER': 'bdadmin',
+        'PASSWORD': 'NGlgEakfLrf8iSAcCOB3',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
